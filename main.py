53d9168ad82ffca989bc05c65768a9915dfde028
Matthew Karasz <mkarasz@cs.nmt.edu>
2016-04-09 18:27:29 -0600

    wat

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import os
 from pygraph.algorithms.searching import depth_first_search
 from datetime import date
 
-lim = 100
+lim = 250
 subswords = dict()
 test = 0
 today = date.today()
 
 while True:
-	fileName = '' + str(today.year) + str(today.month) + str(today.day) 
+	fileName = '' + str(today.year) + str(today.month) + str(today.day)
 	testFileName = fileName + '_' + str(test) + '_' + 'subToWordDict.txt'
 	if( not os.path.isfile('./' + testFileName)):
 		fileName = testFileName
@@ -40,7 +40,7 @@ a.close()
 #get list of subreddits
 subredditFile = open('uscolleges.txt')
 subredditList = subredditFile.read().splitlines()
-subredditsSet = set(subredditList)
+subredditsSet = sorted(subredditList)
 subredditFile.close()
 
 #for reddit API
@@ -50,45 +50,46 @@ for subreddit in subredditsSet:
 	words = set()
 	sub = r.get_subreddit(subreddit)
 	submissions = sub.get_top(limit=lim)
+	time.sleep(5)
 	#gets all the submissions (that we can ) in a subreddit
-	for submission in submissions:
-		if(len(submission.comments) > 0):
-			#reads each comment
-			flat_comments = praw.helpers.flatten_tree(submission.comments)
-			
-			for x in flat_comments:
-				s = x.body
-				#remove punctuation and put lower case
-				comment = s.translate(str.maketrans("","",string.punctuation)).lower()
-				#split into words
-				splitList = sorted(shlex.split(comment))
-				splitSet = set(splitList)
-				
-				#remove all words that are in top 500 words or not in a dictionary
-				for w in common:
-					if w.lower() in splitSet:
-						splitSet.remove(w.lower())
-				
-				toremove = []
-				for w in splitSet:
-					if w not in dict:
-						toremove.append(w)
-					
-				for w in toremove:
-					splitSet.remove(w)
+	try:
+		for submission in submissions:
+			if(len(submission.comments) > 0):
+				#reads each comment
+				flat_comments = praw.helpers.flatten_tree(submission.comments)
 
-				words.update(splitSet)
-				
-	#subswords[subreddit] = words
-	out.write(subreddit + ': ' + repr(sorted(words)) + '\n')
-	
+				for x in flat_comments:
+					s = x.body
+					#remove punctuation and put lower case
+					comment = s.translate(str.maketrans("","",string.punctuation)).lower()
+					#split into words
+					splitList = sorted(shlex.split(comment))
+					splitSet = set(splitList)
+
+					#remove all words that are in top 500 words or not in a dictionary
+					for w in common:
+						if w.lower() in splitSet:
+							splitSet.remove(w.lower())
+
+					toremove = []
+					for w in splitSet:
+						if w not in dict:
+							toremove.append(w)
+
+					for w in toremove:
+						splitSet.remove(w)
+
+					words.update(splitSet)
+
+		#subswords[subreddit] = words
+		out.write(subreddit + ': ' + repr(sorted(words)) + '\n')
+	except praw.errors.Forbidden as e:
+		print(subreddit + ': Forbidden')
+	except praw.errors.InvalidSubreddit as e:
+		print(subreddit + ': Invalid Subreddit')
+	except AttributeError as e:
+		print(subreddit + ': More Comments Error')
 #today = date.today()
 #fileName = '' + str(today.year) + str(today.month) + str(today.day) + 'subToWordDict.txt'
 #out = open(fileName,'w')
 out.close()
-
-
-
-	
-	
-
